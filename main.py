e81bf600349a359e63640c5e250b16ea40b9d808
Yevhen <eug.z2004@gmail.com>
2023-02-16 00:24:13 +0200

    added menu

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 def drawLine():
     print('===========================================')
+
+
+def main():
+    drawLine()
+    print('ROCK PAPER SCISSORS LIZARD SPOCK')
+    drawLine()
+    print('Options')
+    drawLine()
+    print('1. Rock')
+    print('2. Paper')
+    print('3. Scissors')
+    print('4. Lizard')
+    print('5. Spock')
+    drawLine()
+
+
+main()
