948897626402cb6d4e5b4d7e679c94fa9b6a4156
maxpol <mpoluyk@gmail.com>
2023-02-15 23:58:59 +0200

    Computer show victory message method added.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def show_user_victory_message():
     print("Congratulations. You won!")
 
 
+def show_computer_victory_message():
+    print("Sorry, you lost. Good luck next time!")
+
 
 def generate_result(user_choice, computer_choice):
     if user_choice == 'rock' and (computer_choice == 'scissors' or computer_choice == 'lizard'):
