10cfeb11363c8f0a826be88d3b8d85570f8229c1
maxpol <mpoluyk@gmail.com>
2023-02-15 23:57:30 +0200

    User victory method added

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+def show_user_victory_message():
+    print("Congratulations. You won!")
+
+
+
 def generate_result(user_choice, computer_choice):
     if user_choice == 'rock' and (computer_choice == 'scissors' or computer_choice == 'lizard'):
 
